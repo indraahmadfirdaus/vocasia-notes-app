b08a2991f5ee4d5aae05a66dac8babc5e9fb3832
Indra <indraahmadfirdaus@gmail.com>
2024-12-10 19:44:59 +0700

    feat: add enb

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: 'https://vocasia-voca-task-be-production.up.railway.app/api'
+    baseURL: import.meta.env.VITE_API_URL
 })
 
 // Add a request interceptor
